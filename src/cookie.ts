9d49aa9797c43516eff5abf936bd0ef12b2ce639
aarvin roshin <aarvinroshin@gmail.com>
2025-08-09 08:51:28 -0700

    feat(cookie): toast

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,13 +1,30 @@
-import { LaunchProps, LocalStorage } from "@raycast/api";
+import { LaunchProps, LocalStorage, showToast, Toast } from "@raycast/api";
 
 const cookie = async (
 	props: LaunchProps<{
 		arguments: Arguments.Cookie;
 	}>,
-) =>
-	await LocalStorage.setItem(
-		"cookie",
-		props.arguments.cookie,
-	);
+) => {
+	const toast = await showToast({
+		style: Toast.Style.Animated,
+		title: "Saving cookie...",
+	});
+	try {
+		await LocalStorage.setItem(
+			"cookie",
+			props.arguments.cookie,
+		);
+
+		toast.style = Toast.Style.Success;
+		toast.title = "Cookie saved";
+	} catch (err) {
+		toast.style = Toast.Style.Failure;
+		toast.title = "Failed to save cookie";
+
+		if (err instanceof Error) {
+			toast.message = err.message;
+		}
+	}
+};
 
 export default cookie;
